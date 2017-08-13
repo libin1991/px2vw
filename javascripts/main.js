485e230549514c76a28ac3a880e5a8e1ff8f6627
hezedu@hotmail.com <hezedu@hotmail.com>
2017-08-13 09:57:10 +0800

    1.0.2

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,15 @@
 (function() {
   //****************px2vw start****************
-  //https://github.com/hezedu/px2vw
   var WIDTH = 320;
   //,FIXED = 5;
 
   var REG = /([1-9]\d*\.\d*|0\.\d*[1-9]\d|\d)+px/gi; //去零正则表达式
 
-  function trimEnd0(str) { //去掉未尾多余的0.
-    str = str.replace(/0+$/, '');
-    var lastIndex = str.length - 1;
-    return str[lastIndex] !== '.' ? str : str.substr(0, lastIndex);
-  }
+  // function trimEnd0(str) { //去掉未尾多余的0.
+  //   str = str.replace(/0+$/, '');
+  //   var lastIndex = str.length - 1;
+  //   return str[lastIndex] !== '.' ? str : str.substr(0, lastIndex);
+  // }
 
   function matchCtrl(width) {
     return function(m) { //replace匹配字符串处理
@@ -18,7 +17,8 @@
       m = Number(m);
       m = (m / width) * 100;
       //m = m.toFixed(fixed);
-      return trimEnd0(m.toString()) + 'vw';
+      //return trimEnd0(m.toString()) + 'vw';
+      return m + 'vw';
     }
   }
 
@@ -26,6 +26,7 @@
     width = width || WIDTH;
     return str.replace(REG, matchCtrl(width));
   }
+
   //****************px2vw end****************
 
   function $(id){
